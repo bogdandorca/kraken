e69770eada1cfe6e8fd447b24ae48481f91a72ee
Bogdan <bdorca@webassign.net>
2016-04-06 11:47:38 +0300

    Client application cleared

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,8 +1,3 @@
 import 'reflect-metadata';
 
 import { bootstrap } from 'angular2/platform/browser';
-import { VehicleComponent } from './modules/vehicle/vehicle.component';
-import { UsernameComponent } from './modules/username/username.component';
-
-bootstrap(VehicleComponent);
-bootstrap(UsernameComponent);
